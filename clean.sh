4fb176210aaa58082508e204eef20c057ca88dd3
Balint Joo <bjoo@jlab.org>
2019-07-23 12:58:29 -0400

    Fixed the cleanup script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,14 @@
-rm -rf ./build && mkdir ./build
-rm -rf ./install && mkdir ./install
+#!/bin/bash
 
+pushd  ./build
+rm -rf ./build_cpp_wilson_dslash
+rm -rf ./build_kokkos_dslash
+rm -rf ./build_libxml2
+rm -rf ./build_qdp++-scalar
+popd
+ 
+pushd ./install
+rm -rf ./cpp_wilson_dslash
+rm -rf ./libxml2
+rm -rf ./qdp++-scalar
+popd
