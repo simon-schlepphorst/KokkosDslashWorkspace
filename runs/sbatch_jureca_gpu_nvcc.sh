76fc5e27c15b01915c111ce0992bb977ca4cecaa
Simon Schlepphorst <dev@simon-schlepphorst.de>
2022-08-30 02:04:50 +0200

    Sbatch templates

--- runs/sbatch_jureca_gpu_nvcc.sh
+++ runs/sbatch_jureca_gpu_nvcc.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=256
-#SBATCH --partition=batch
+#SBATCH --partition=dc-gpu
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
-srun --exclusive --cpu-bind=none ./../build/build_kokkos_dslash/test/test_kokkos_perf_fp
+srun --exclusive --cpu-bind=none "$1"
---
+++ runs/sbatch_jusuf_clang.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+#SBATCH --account=slnpp
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=256
+#SBATCH --partition=batch
+
+module load Clang
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+srun --exclusive --cpu-bind=none "$1"
