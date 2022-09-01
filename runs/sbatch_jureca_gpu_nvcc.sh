167436a1a52f6c715431e8577054067bb8a1b1a2
Simon Schlepphorst <dev@simon-schlepphorst.de>
2022-09-02 01:58:02 +0200

    fix benchmarks + setup

--- runs/sbatch_jureca_gpu_nvcc.sh
+++ runs/sbatch_jureca_gpu_nvcc.sh
@@ -5,6 +5,7 @@
 #SBATCH --cpus-per-task=256
 #SBATCH --gres=gpu:1 --partition=dc-gpu
 
+module load CUDA
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
