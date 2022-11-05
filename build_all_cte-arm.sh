4164c349aabbefe5413bb2a454867ba3e5d53483
Simon Schlepphorst <dev@simon-schlepphorst.de>
2022-11-05 05:14:31 +0100

    ARM Setup

---
+++ build_all_cte-arm.sh
@@ -0,0 +1,5 @@
+./build_libxml2.sh
+./build_qdp++-scalar.sh
+./build_kokkos_dslash_arm.sh
+./build_kokkos_dslash_arm_mdrange.sh
+./build_kokkos_dslash_arm_flat.sh
--- env_arm_clang14.0.sh
+++ env_arm_clang14.0.sh
@@ -1,15 +1,16 @@
 module load cmake/3.24.2_arm
 module load gcc/11.1
+module load llvm/14.0-git-openmp
 
 TOPDIR=`pwd`
 
-BUILDDIR=${TOPDIR}/build/gcc
-INSTALLDIR=${TOPDIR}/install/gcc
+BUILDDIR=${TOPDIR}/build/clang
+INSTALLDIR=${TOPDIR}/install/clang
 SRCDIR=${TOPDIR}/src
 
 PK_ARCH="aarch64-unknown-linux"
-PK_CXX="g++"
-PK_CC="gcc"
+PK_CXX="clang++"
+PK_CC="clang"
 PK_OMP_ENABLE="--enable-openmp"
 PK_CXXFLAGS=" -g -O3 -std=c++14 -fopenmp -march=native"
 PK_CFLAGS="-g -O3 -std=gnu99 -fopenmp"
